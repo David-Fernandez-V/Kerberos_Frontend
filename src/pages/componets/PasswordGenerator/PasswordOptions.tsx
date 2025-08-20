e2c4e48c03c5bc34fc288a8138cdf578f360bfb9
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-08-20 12:53:07 -0600

    Eliminación y ajustes en los parámetros para la generación de contraseñas

--- src/pages/componets/PasswordGenerator/PasswordOptions.tsx
+++ src/pages/componets/PasswordGenerator/PasswordOptions.tsx
@@ -22,62 +22,16 @@ type Props = {
 function PasswordOptions({ onChange }: Props) {
   //Valores por defecto
   const [config, setConfig] = useState<PasswordConfig>({
-    length: 12,
+    length: 16,
     include_capital: true,
     include_lower: true,
     include_number: true,
     include_symbols: true,
-    quantity_numbers: 5,
-    quantity_symbols: 5,
   });
-
-  //Estados para la coherencia de los inputs
-  const [numCount, setNumCount] = useState(5);
-  const [symbolCount, setSymbolCount] = useState(5);
-  const [includeNumbers, setIncludeNumbers] = useState(true);
-  const [includeSymbols, setIncludeSymbols] = useState(true);
-
-  //Funciones para la coherencia de los inputs
-  const handleNumChange = (input: number) => {
-    const val = Math.max(0, input);
-    setNumCount(val);
-    setConfig({ ...config, quantity_numbers: val });
-
-    if (val > 0 && !includeNumbers) {
-      setIncludeNumbers(true);
-    }
-    if (val === 0 && includeNumbers) {
-      setIncludeNumbers(false);
-    }
-  };
-
-  const handleSymbolChange = (input: number) => {
-    const val = Math.max(0, input);
-    setSymbolCount(val);
-    setConfig({ ...config, quantity_symbols: val });
-
-    if (val > 0 && !includeSymbols) {
-      setIncludeSymbols(true);
-    }
-    if (val === 0 && includeSymbols) {
-      setIncludeSymbols(false);
-    }
-  };
-
-  const handleNumChekbox = (checked: boolean) => {
-    setIncludeNumbers(checked);
-    setNumCount(checked ? 5 : 0);
-    setConfig({ ...config, include_number: checked });
-  };
-
-  const handleSymbolCheckbox = (checked: boolean) => {
-    setIncludeSymbols(checked);
-    setSymbolCount(checked ? 5 : 0);
-    setConfig({ ...config, include_symbols: checked });
-  };
 
   //Efecto para actualizar con cada cambio
   useEffect(() => {
+    console.log(config);
     onChange(config);
   }, [config]);
 
@@ -130,55 +84,23 @@ function PasswordOptions({ onChange }: Props) {
           </FormControl>
           {/*Números*/}
           <FormControl id="digits">
-            <FormLabel>Números</FormLabel>
+            <FormLabel>Dígitos</FormLabel>
             <Checkbox
-              isChecked={includeNumbers}
-              onChange={(e) => handleNumChekbox(e.target.checked)}
+              isChecked={config.include_number}
+              onChange={(e) =>
+                setConfig({ ...config, include_number: e.target.checked })
+              }
             />
           </FormControl>
           {/*Símbolos*/}
           <FormControl id="simbols">
             <FormLabel>Símbolos</FormLabel>
             <Checkbox
-              isChecked={includeSymbols}
-              onChange={(e) => handleSymbolCheckbox(e.target.checked)}
+              isChecked={config.include_symbols}
+              onChange={(e) =>
+                setConfig({ ...config, include_symbols: e.target.checked })
+              }
             />
-          </FormControl>
-        </HStack>
-
-        <HStack mt={5}>
-          {/*Cantidad de números*/}
-          <FormControl id="digits_number" mb={5}>
-            <FormLabel>Cantida de dígitos</FormLabel>
-            <NumberInput
-              value={numCount}
-              onChange={(_, v) => handleNumChange(v)}
-              //min={config.include_number ? 1 : 0}
-              min={0}
-            >
-              <NumberInputField borderRightRadius={5} borderLeftRadius={5} />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-
-          {/*Cartidad de símbolos*/}
-          <FormControl id="simbols_number" mb={5}>
-            <FormLabel>Cantidad de símbolos</FormLabel>
-            <NumberInput
-              value={symbolCount}
-              onChange={(_, v) => handleSymbolChange(v)}
-              //min={config.include_number ? 1 : 0}
-              min={0}
-            >
-              <NumberInputField borderRightRadius={5} borderLeftRadius={5} />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
           </FormControl>
         </HStack>
       </Feature>
