b69a09eb4a8cd100bc12fa1f7068cfe70f68ef21
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-09-01 20:11:37 -0600

    Implementación de borrar tarjeta full

---
+++ src/pages/Dashboard/Cards/CardDeleteSecurity.tsx
@@ -0,0 +1,150 @@
+import {
+  Text,
+  Divider,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  ModalFooter,
+  Button,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import { useForm } from "react-hook-form";
+import { mpwdSchema, mpwdForm } from "../../../schemas/mpwdSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import CardConfirmation from "./CardConfirmation";
+import { CardItem } from "../../../types";
+import useCheckMasterpwd from "../../../hooks/useCheckMasterpwd";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  card: CardItem | null;
+};
+
+function CardDeleteSecurity({ isOpen, onClose, card }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<mpwdForm>({
+    resolver: zodResolver(mpwdSchema),
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [Mpwd, setMpwd] = useState<string | null>(null);
+
+  const ConfirmationAlert = useDisclosure();
+  const { mutate } = useCheckMasterpwd();
+
+  useEffect(() => {
+    if (!isOpen) {
+      reset(); // Limpia los campos del formulario
+      setShowPassword(false);
+      setErrorMessage("");
+    }
+  }, [isOpen]);
+
+  const onSubmit = (formData: mpwdForm) => {
+    if (card) {
+      mutate(
+        { master_password: formData.mpwd },
+        {
+          onSuccess: (data) => {
+            setMpwd(formData.mpwd);
+            console.log(data);
+            ConfirmationAlert.onOpen();
+            onClose();
+          },
+          onError: () => {
+            setErrorMessage("Contraseña incorrecta");
+          },
+        }
+      );
+    } else {
+      console.log("Sin tarjeta seleccionada");
+    }
+  };
+
+  return (
+    <>
+      <CardConfirmation
+        isOpen={ConfirmationAlert.isOpen}
+        onClose={ConfirmationAlert.onClose}
+        card={card}
+        masterPwd={Mpwd}
+        onCloseDetail={onClose}
+      />
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size="xl"
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader color="purple.700">
+            Confirmación de Seguridad
+          </ModalHeader>
+          <ModalCloseButton />
+          <Divider />
+          <ModalBody>
+            <form id="mpwdForm" onSubmit={handleSubmit(onSubmit)}>
+              <FormControl id="mpwd">
+                <FormLabel>Vuelva a introducir la contraseña maestra</FormLabel>
+                <InputGroup>
+                  <Input
+                    id="pwd"
+                    type={showPassword ? "text" : "password"}
+                    {...register("mpwd")}
+                    onFocus={() => setErrorMessage("")}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <IconButton
+                      aria-label="Search database"
+                      mr={2}
+                      variant="ghost"
+                      h="1.75rem"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </IconButton>
+                  </InputRightElement>
+                </InputGroup>
+                {errors?.mpwd?.message && (
+                  <Text color={"red.600"}>{errors?.mpwd?.message}.</Text>
+                )}
+                {errorMessage && <Text color="red.600">{errorMessage}.</Text>}
+              </FormControl>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button form="mpwdForm" type="submit" mr={3}>
+              Aceptar
+            </Button>
+            <Button variant="ghost" onClick={onClose}>
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default CardDeleteSecurity;
