c21bc8606fead541de2ae46fcb03d9d5173da62c
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-09-11 18:08:30 -0600

    Corrección de skeletons

--- src/pages/componets/PrivateSkeleton.tsx
+++ src/pages/componets/PrivateSkeleton.tsx
@@ -1,36 +1,71 @@
-import { Skeleton, Stack } from "@chakra-ui/react";
-import Feature from "./Feature";
+import { Flex, Skeleton } from "@chakra-ui/react";
 
 type Props = {};
 
 function PrivateSkeleton({}: Props) {
   return (
-    <div>
-      <Feature title=" ">
-        <Stack>
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-        </Stack>
-      </Feature>
-      <br />
-      <Feature title=" ">
-        <Stack>
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-        </Stack>
-      </Feature>
-      <br />
-      <Feature title=" ">
-        <Stack>
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-        </Stack>
-      </Feature>
-      <br />
-    </div>
+    <Flex
+      h="100vh"
+      w="100%"
+      bg="gray.50"
+      direction={{ base: "column", md: "row" }}
+    >
+      <Skeleton
+        display={{ base: "none", md: "block" }}
+        w="15%"
+        h="100%"
+        borderRadius="md"
+      />
+
+      <Flex
+        direction="column"
+        w={{ base: "100%", md: "85%" }}
+        p={4}
+        gap={4}
+        flex={1}
+      >
+        <Flex
+          h={{ base: "10%", md: "10%" }}
+          align="center"
+          gap={4}
+          direction={{ base: "row", md: "row" }}
+        >
+          <Skeleton
+            display={{ base: "block", md: "none" }}
+            flex={1}
+            h="40px"
+            borderRadius="md"
+          />
+          <Skeleton
+            display={{ base: "block", md: "none" }}
+            boxSize="40px"
+            borderRadius="full"
+          />
+          <Skeleton
+            display={{ base: "none", md: "block" }}
+            h="100%"
+            flex={1}
+            borderRadius="md"
+          />
+          <Skeleton
+            display={{ base: "none", md: "block" }}
+            h="100%"
+            flex={1}
+            borderRadius="md"
+          />
+          <Skeleton
+            display={{ base: "none", md: "block" }}
+            boxSize="50px"
+            borderRadius="full"
+          />
+        </Flex>
+        <Flex direction="column" flex={1} gap={4}>
+          <Skeleton flex={1} borderRadius="md" />
+          <Skeleton flex={1} borderRadius="md" />
+          <Skeleton flex={1} borderRadius="md" />
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
--- src/pages/componets/PublicSkeleton.tsx
+++ src/pages/componets/PublicSkeleton.tsx
@@ -1,36 +1,18 @@
-import { Skeleton, Stack } from "@chakra-ui/react";
-import Feature from "./Feature";
+import { Flex, Skeleton } from "@chakra-ui/react";
 
 type Props = {};
 
 function PublicSkeleton({}: Props) {
   return (
-    <div>
-      <Feature title=" ">
-        <Stack>
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-        </Stack>
-      </Feature>
-      <br />
-      <Feature title=" ">
-        <Stack>
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-        </Stack>
-      </Feature>
-      <br />
-      <Feature title=" ">
-        <Stack>
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-          <Skeleton height="50px" />
-        </Stack>
-      </Feature>
-      <br />
-    </div>
+    <Flex h="100vh" w="100%" bg="gray.50" direction="column" p={4} gap={4}>
+      <Skeleton h={{ base: "10%", md: "60px" }} w="100%" borderRadius="md" />
+
+      <Flex flex={1} gap={4} direction={{ base: "column", md: "row" }}>
+        <Skeleton flex={1} borderRadius="md" />
+        <Skeleton flex={1} borderRadius="md" />
+        <Skeleton flex={1} borderRadius="md" />
+      </Flex>
+    </Flex>
   );
 }
 
