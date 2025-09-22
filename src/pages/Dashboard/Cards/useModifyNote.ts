0eef03c8da69f93fdd02d28c64efbf6c7fefdb8b
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-09-22 13:27:42 -0600

    Implementación de modificación de notas

---
+++ src/pages/Dashboard/Cards/useModifyNote.ts
@@ -0,0 +1,62 @@
+import { useMutation } from "@tanstack/react-query";
+import { noteForm } from "../../../schemas/noteSchema";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+
+interface ModifyNoteRequest {
+  new_data: noteForm;
+  note_request: {
+    note_id: number;
+    master_password?: string;
+  };
+}
+
+interface ModifyNoteResponse {
+  confirmation: string;
+}
+
+const API_URL = import.meta.env.VITE_API_URL;
+const url = `${API_URL}/notes/modify`;
+
+export default function useModifyNote() {
+  const toast = useToast();
+
+  return useMutation<ModifyNoteResponse, unknown, ModifyNoteRequest>({
+    mutationFn: async ({ new_data, note_request }) => {
+      const response = await axios.post<ModifyNoteResponse>(
+        url,
+        {
+          new_data,
+          note_request,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      return response.data;
+    },
+    onSuccess: (response) => {
+      toast({
+        title: "Nota modificada",
+        description:
+          response.confirmation || "Los datos se modificaron correctamente.",
+        status: "success",
+        duration: 4000,
+        isClosable: true,
+        position: "top",
+      });
+    },
+    onError: (error: any) => {
+      toast({
+        title: "Error",
+        description:
+          error.response?.data?.detail || "No se pudo modificar la nota",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+        position: "top-right",
+      });
+    },
+  });
+}
