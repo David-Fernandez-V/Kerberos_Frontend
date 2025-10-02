03718bac87df6cc2744c8810004a463fc8810b93
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-10-02 12:46:08 -0600

    Implementación para la solicitud de cambio de correo

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,7 +4,10 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
   Spinner,
   Text,
 } from "@chakra-ui/react";
@@ -19,7 +22,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import useChangeName from "./useChangeName";
 import useSettings from "../../states/SettingsStore";
 import useProfile from "../componets/SideBar/useProfile";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+  ChangeEmailForm,
+  ChangeEmailSchema,
+} from "../../schemas/changeEmailSchema";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import useRequestChangeEmail from "./useRequestChangeEmail";
 
 type Props = {};
 
@@ -27,27 +36,58 @@ function Settings({}: Props) {
   const { data: user } = useProfile();
   const { username, email, setEmail } = useSettings();
 
-  const { mutate, isPending: isNamePending } = useChangeName();
+  const {
+    mutate: requestEmailChange,
+    isPending: isEmailPending,
+    isSuccess: isEmailSucces,
+  } = useRequestChangeEmail();
+  const { mutate: changeName, isPending: isNamePending } = useChangeName();
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
+    register: registerName,
+    handleSubmit: habldeSubmitName,
+    formState: { errors: nameErrors },
+    setError: setNameErrors,
   } = useForm<ChangeNameForm>({
     resolver: zodResolver(ChangeNameSchema),
   });
 
+  const {
+    register: registerEmail,
+    handleSubmit: handleSubmitEmail,
+    formState: { errors: emailErrors },
+    setError: setEmailError,
+  } = useForm<ChangeEmailForm>({
+    resolver: zodResolver(ChangeEmailSchema),
+  });
+
   const handleChangeName = (data: ChangeNameForm) => {
     if (data.new_name === username) {
-      setError("new_name", {
+      setNameErrors("new_name", {
         type: "manual",
         message: "Sin cambios",
       });
       return;
     }
 
-    mutate({ new_name: data.new_name });
+    changeName({ new_name: data.new_name });
+  };
+
+  const handleChangeEmail = (data: ChangeEmailForm) => {
+    if (data.new_email === email) {
+      setEmailError("new_email", {
+        type: "manual",
+        message: "Sin cambios",
+      });
+      return;
+    }
+
+    requestEmailChange({
+      new_email: data.new_email,
+      master_password: data.master_password,
+    });
   };
 
   useEffect(() => {
@@ -61,18 +101,15 @@ function Settings({}: Props) {
       <Flex gap={4} direction="column" wrap="wrap" maxW={"30%"}>
         {/*Cambio de nombre */}
         <Feature title="Cambiar nombre">
-          <form
-            action="changeNameForm"
-            onSubmit={handleSubmit(handleChangeName)}
-          >
+          <form onSubmit={habldeSubmitName(handleChangeName)}>
             <FormControl>
               <Input
                 type="text"
                 defaultValue={username}
-                {...register("new_name")}
+                {...registerName("new_name")}
               />
-              {errors?.new_name?.message && (
-                <Text color={"red.600"}>{errors?.new_name?.message}</Text>
+              {nameErrors?.new_name?.message && (
+                <Text color={"red.600"}>{nameErrors?.new_name?.message}</Text>
               )}
             </FormControl>
             <br />
@@ -91,26 +128,51 @@ function Settings({}: Props) {
             variant="flushed"
             isReadOnly
             value={email}
-            //{...register("new_name")}
             mb={2}
           />
           <Divider mb={2} mt={2} />
-          <form>
+          <form onSubmit={handleSubmitEmail(handleChangeEmail)}>
             <FormControl>
               <FormLabel>Contraseña maestra</FormLabel>
-              <Input type="password" />
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  pr="3rem"
+                  {...registerEmail("master_password")}
+                />
+                <InputRightElement width="4.5rem">
+                  <IconButton
+                    aria-label="Cambiar visibilidad"
+                    variant="ghost"
+                    h="1.75rem"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </IconButton>
+                </InputRightElement>
+              </InputGroup>
+              {emailErrors?.master_password?.message && (
+                <Text color={"red.600"}>
+                  {emailErrors.master_password?.message}
+                </Text>
+              )}
             </FormControl>
             <FormControl mt={3}>
               <FormLabel>Nuevo correo</FormLabel>
               <Input
                 type="text"
                 placeholder={"correo@ejemplo.com"}
-                //{...register("new_name")}
+                {...registerEmail("new_email")}
               />
+              {emailErrors?.new_email?.message && (
+                <Text color={"red.600"}>{emailErrors.new_email.message}</Text>
+              )}
             </FormControl>
             <br />
-            {isNamePending ? (
+            {isEmailPending ? (
               <Spinner />
+            ) : isEmailSucces ? (
+              <></>
             ) : (
               <Button type="submit">Cambiar</Button>
             )}
